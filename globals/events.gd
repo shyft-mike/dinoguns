5c6c5e85bdab0ae946d554397f42ee5a41024bab
Michael Ferguson <mike@shyftsolutions.io>
2023-09-02 13:31:02 -0500

    UI work

--- globals/events.gd
+++ globals/events.gd
@@ -3,3 +3,4 @@ extends Node
 
 signal character_selected (PlayerCharacter)
 signal experience_received (int)
+signal event_logged (String)
--- story_mode.gd
+++ story_mode.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _ready():
-	$SubViewportContainer/SubViewport/Panel/Player._character = State.selected_character
+	$Player._character = State.selected_character
